e102df41a3e503cfd06cd7fabbe7095bdbecc27b
Allyana Alombro <aalombro@f0r6s8.codam.nl>
2025-11-27 19:39:09 +0100

    finished ex06

--- CPP_01/ex06/Harl.cpp
+++ CPP_01/ex06/Harl.cpp
@@ -6,7 +6,7 @@
 /*   By: aalombro <aalombro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/27 18:26:47 by aalombro          #+#    #+#             */
-/*   Updated: 2025/11/27 18:55:56 by aalombro         ###   ########.fr       */
+/*   Updated: 2025/11/27 19:32:54 by aalombro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,48 +15,65 @@
 
 Harl::Harl()
 {
-	std::cout << ">>>>>Welcome to Harldonalds, how may we help you today?<<<<<\n";
 }
 
 Harl::~Harl()
 {
-	std::cout << ">>>>>Thank you for coming to Harldonalds, goodbyeee..... <<<<<\n";
 }
 
 void Harl::debug(void)
 {
+	std::cout << "[ DEBUG ]\n";
 	std::cout << "I love having extra bacon for my" << 
 		"7XL-double-cheese-triple-pickle-special-ketchup burger. I really do!\n";
 }
 
 void Harl::info(void)
 {
+	std::cout << "[ INFO ]\n";
 	std::cout << "I cannot believe adding extra bacon costs more money.\n" <<
 	 "You did not put enough bacon in my burger! If you did, I wouldn’t be asking for more!\n";
 }
 
 void Harl::warning(void)
 {
+	std::cout << "[ WARNING ]\n";
 	std::cout << "I think I deserve to have some extra bacon for free.\n" <<
 		"I’ve been coming for years, whereas you started working here just last month.\n";
 }
 
 void Harl::error(void)
 {
+	std::cout << "[ ERROR ]\n";
 	std::cout << "This is unacceptable!!!!! I want to speak to the manager now.\n";
 }
 
 void Harl::complain(std::string level)
 {
 	std::string	levels[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
-	void (Harl::*functions[4])(void) = {&Harl::debug, &Harl::info, &Harl::warning, &Harl::error};
-
+	int index = -1;
+	
 	for (int i = 0; i < 4; i++)
 	{
 		if (levels[i] == level)
 		{
-			(this->*functions[i])();
-			return;
+			index = i;
+			break;
 		}
+	}
+
+	switch (index)
+	{
+		case 0:
+			debug();
+		case 1:
+			info();
+		case 2:
+			warning();
+		case 3:
+			error();
+			break;
+		default:
+			std::cout << "[ Probably complaining about insignificant problems ]\n";
 	}
 }
--- CPP_01/ex06/main.cpp
+++ CPP_01/ex06/main.cpp
@@ -6,14 +6,23 @@
 /*   By: aalombro <aalombro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/27 18:26:22 by aalombro          #+#    #+#             */
-/*   Updated: 2025/11/27 19:00:34 by aalombro         ###   ########.fr       */
+/*   Updated: 2025/11/27 19:38:16 by aalombro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Harl.hpp"
+#include <iostream>
 
 int main(int argc, char **argv)
 {
-	std::string complaint = *argv;
+	Harl	harl;
 	
+	if (argc == 1)
+		std::cout << "What? Nothing to say? Pick DEBUG, INFO, WARNING, OR ERROR.";
+	else
+	{
+		std::string complaint = argv[1];
+		harl.complain(complaint);
+	}
+
 }
