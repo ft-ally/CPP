6b915fb20c3d711ad9ca70926ac6f72c9ab8989b
Allyana Alombro <Allygsa@gmail.com>
2026-08-11 11:36:43 +0200

    08/02 class setup orthodox

--- CPP_08/ex02/MutantStack.hpp
+++ CPP_08/ex02/MutantStack.hpp
@@ -1,4 +1,13 @@
 #ifndef MUTANT_STACK
 #define MUTANT_STACK
 
+class Mutant
+{
+	public:
+		Mutant();
+		Mutant(const Mutant &src);
+		Mutant& operator=(const Mutant &src);
+		~Mutant();
+};
+
 #endif
